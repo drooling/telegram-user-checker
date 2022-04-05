65533e6b23d0ca737a2fae061e79c2d3ab0a8550
Surtains <97707885+9sv@users.noreply.github.com>
2022-04-05 15:23:25 +0000

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,45 @@
 use std::fs::{self, File};
+use std::env;
+use std::path;
+use rand::Rng;
 use std::io::Write;
+
+fn get_outfile() -> std::fs::File {
+    if !path::Path::new("./results").exists() {
+        fs::create_dir("results").expect("Coudlnt create `results` dir");
+    }
+    if path::Path::new("./results/free_users.txt").exists() {
+        fs::rename("./results/free_users.txt", format!("./results/free_users_{:?}.txt", rand::thread_rng().gen_range(0..100))).expect("Couldnt rename existing `free_users.txt` file");
+        return File::create("./results/free_users.txt").unwrap();
+    } else {
+        return File::create("./results/free_users.txt").unwrap();
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     const MATCH: &str = "class=\"tgme_page_additional\"";
+    const INVALID_CHARS: [char; 32] = ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
 
-    let users = fs::read_to_string("users.txt").expect("Error loading file");
-    let mut of = File::create("valid_users.txt").unwrap();
+    let args: Vec<String> = env::args().collect();
+    let users_file;
+    if args.len() > 1{
+        users_file = String::from(&args[1]);
+    } else {
+        users_file = String::from("users.txt");
+    }
+
+    let users = fs::read_to_string(&users_file).expect("Error loading input file");
+    let mut of = get_outfile();
+
+    println!("[ ! ] Loaded ( {} ) Usernames", &users.lines().count());
 
     for user in users.lines() {
         if user.len() < 5 {
+            continue;
+        } else if !user.is_ascii() {
+            continue;
+        } else if user.chars().any(|c| INVALID_CHARS.contains(&c)) {
             continue;
         }
         let resp = reqwest::get(format!("https://t.me/{}/", user))
@@ -18,12 +48,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .await?;
         if !resp.contains(MATCH) {
             println!("[ + ] t.me/{} -- FREE / BANNED", user);
-            match writeln!(&mut of, "{}", format!("{}", user)) {
+            match writeln!(of, "{}", format!("{}", user)) {
                 Ok(r) => r,
                 Err(_) => continue,
             };
         }
     }
+
+    println!("Finished checking {}", users_file);
 
     Ok(())
 }
